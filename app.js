97508a43114c73cb91a53d4aa60b34ca0b3d38b7
Tim Savery <tim.savery@gmail.com>
2013-03-08 16:34:46 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var stylus = require('stylus')
+  , express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ 
+    dumpExceptions: true, 
+    showStack: true 
+  }));
+});
+
+app.get('/', routes.index);
+
+app.listen(3000, function() {
+  console.log('server started on port 3000');
+});
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+exports.index = function(req, res){
+  res.render('index', { 
+  	title: '',
+  	layout: false
+  });
+};
